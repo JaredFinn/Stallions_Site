6e88c5cfd8ed8b0b764aac352cd5acff45525bdf
Jared Finn <jaredhfinn@gmail.com>
2021-10-01 13:53:05 -0400

    new footer

--- stallions-theme/footer.php
+++ stallions-theme/footer.php
@@ -11,7 +11,38 @@
 	
 	</div>
 <?php // footer // ?>
-        <footer class="footerw">
+		<footer class="footerw">
+			<img id="logo" src="https://stallionsv2.fishcreekrent.com/wp-content/uploads/2021/10/pg-logo-1.png">
+			<div class="nav">
+				<ul class="nav-list">
+				<li class="nav-item"><a class="item-link" href="/">Home</a></li>
+				<p id="lines">|</p>
+				<li class="nav-item"><a class="item-link" href="/team/roster/">Team</a></li>
+				<p id="lines">|</p>
+				<li class="nav-item"><a class="item-link" href="/news/">News</a></li>
+				<p id="lines">|</p>
+				<li class="nav-item"><a class="item-link" href="/event/">Schedule</a></li>
+				<p id="lines">|</p>
+				<li class="nav-item"><a class="item-link" href="/tickets/info">Tickets</a></li>
+				<p id="lines">|</p>
+				<li class="nav-item"><a class="item-link" href="http://shop.saugertiesstallions.com/">Store</a></li>
+				</ul>
+			</div>
+			<div class="site-general">
+				<ul class="site-general-nav">
+				<li class="general-nav-item"><a class="general-link" href="">Terms of Service</a></li>
+				<p id="dash">-</p>
+				<li class="general-nav-item"><a class="general-link" href="">Privacy Policy</a></li>
+				<p id="dash">-</p>
+				<li class="general-nav-item"><a class="general-link" href="/contact/">Contact Us</a></li>
+				</ul>
+			</div>
+			<div class="copyright-element">
+				<h4 id="copyright">©2021 Saugerties Stallions. All rights reserved.</h4>
+			</div>
+		</footer>
+
+        <!--<footer class="footerw">
             <div class="copyright">
                 ©2021 Saugerties Stallions. All rights reserved.
             </div>
@@ -23,9 +54,9 @@
 			
 			<div class="bottomnav">
 				<nav>
-					<!--<a>Privacy Policy</a> <a>Terms of Service</a> <a>Contact Us</a><a>About Us</a>-->
+					<a>Privacy Policy</a> <a>Terms of Service</a> <a>Contact Us</a><a>About Us</a>
 				</nav>
 			</div>
-        </footer>
+        </footer>-->
 		
 </html>
